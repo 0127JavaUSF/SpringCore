0d8f752906b8ea9571806bc75f4f29944d0c70fb
unknown <amilevich@yahoo.com>
2020-03-03 13:04:52 -0500

    spring core after lunch

--- HelloSpring/src/main/java/com/example/service/VillainServiceImpl.java
+++ HelloSpring/src/main/java/com/example/service/VillainServiceImpl.java
@@ -8,6 +8,16 @@ import org.springframework.stereotype.Service;
 import com.example.dao.VillainDao;
 import com.example.model.Villain;
 
+
+/*
+ * Stereotype annotation - annotation above a class definition that
+ * indicates that it is a bean Spring can manage
+ * @Component(parent)
+ * @Repository
+ * @Controller
+ * @Service
+ * @Configuration
+ */
 @Service("villainService")
 public class VillainServiceImpl implements VillainService {
 	
@@ -24,6 +34,20 @@ public class VillainServiceImpl implements VillainService {
 	
 	public VillainServiceImpl() {
 		System.out.println("in no args constructor");
+	}
+	
+	@Autowired
+	//the autowired annotaion tells Spring to attempt autowiring
+	//using the below method, aka our setter
+	public void setVillainDao(VillainDao vDao) {
+		System.out.println("in setter");
+		this.villainDao = vDao;
+	}
+	
+	@Autowired
+	public VillainServiceImpl(VillainDao vDao) {
+		System.out.println("In DI constructor");
+		this.villainDao = vDao;
 	}
 
 }
